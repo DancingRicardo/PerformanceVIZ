d83e7e31f29d2e29ee7fc8b03a876cf54de2cd52
Bradley Fernandez <99092565+DancingRicardo@users.noreply.github.com>
2024-01-09 10:00:02 -0500

    Saving Function working
    Added Function that can load the terminal output and save it to any file the user likes. Will find the terminal output file path and put it in the instructions.

--- terminalgrabber.py
+++ terminalgrabber.py
@@ -1,1 +1,19 @@
-# Here will be a class that grabs the file output of the terminal and saves it to the computer system.
+
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+
+tk.Tk().withdraw() # Says that you won't be using many tkinter functions
+
+
+def saveResults():
+    # Open the file inside the project path, appending it
+
+        fileToLoad = open(askopenfilename(), 'r')
+        # Save the CODE variable to the User's C++ Project.
+        print(fileToLoad)
+        fileToSave = open(askopenfilename(), 'a')
+        fileToSave.write(fileToLoad.read())
+        print(fileToSave)
+        fileToSave.close()
+        print("Saved!")
+  
