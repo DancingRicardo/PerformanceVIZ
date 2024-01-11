ec6eed78c79b29ea4c0e7d59ee173ac5165bd523
Bradley Fernandez <99092565+DancingRicardo@users.noreply.github.com>
2024-01-11 00:12:27 -0500

    Update main.py
    Updated main to have the current intended functionality of this application.

--- main.py
+++ main.py
@@ -5,14 +5,48 @@
 
 import numpy as np
 import graphics
+import terminalgrabber as tg
+import pygame as pg
+import pygame_textinput as textinput
 
-performanceContainer = np.array([[0,0], [3,0], [6,3], [9,1], [12,2], [15,0], [18,3], [21,1]])
+pg.init()
+
+performanceContainer = np.array([])
+
+# Create TextInput-object
+textObj = textinput.TextInputVisualizer()
+
+screen = pg.display.set_mode((1000, 700))
+clock = pg.time.Clock()
+
+while True:
+    screen.fill((225, 225, 225))
+
+    events = pg.event.get()
+
+    # Feed it with events every frame
+    textObj.update(events)
+    # Blit its surface onto the screen
+    screen.blit(textObj.surface, (10, 10))
+
+    for event in events:
+        if event.type == pg.QUIT:
+            exit()
+        if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
+            exit()
+    buffer = tg.readPerformance(textObj.value)
+    performanceContainer = np.append(performanceContainer, buffer)
+
+    pg.display.update()
+    clock.tick(30)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 print(performanceContainer.itemsize)
 graph = graphics.LineGraph('TestGraph', performanceContainer, 8)
 
+tg.saveResults()
+
 graph.draw()
 
-print('test')
+
